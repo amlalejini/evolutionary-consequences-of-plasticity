43a6a2173dd48bfc4be1e3a9c33208f09f04b6f0
Austin Ferguson <ferguson.austin.j@gmail.com>
2020-08-31 10:56:24 -0400

    Add straggling static evolvability files

---
+++ experiments/2020-08-19-static-evolvability/hpcc/analysis_job_gen.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 gen_sub_multi.py --config_dir /mnt/home/fergu358/research/plasticity/evolvability/experiments/2020-08-19-static-evolvability/hpcc/config/ --data_dir /mnt/gs18/scratch/users/fergu358/plasticity/evolvability/2020-08-19-static_evolvability/data/ --job_dir /mnt/gs18/scratch/users/fergu358/plasticity/evolvability/2020-08-19-static_evolvability/analysis_jobs/ --analysis_file /mnt/home/fergu358/research/plasticity/evolvability/experiments/2020-08-19-static-evolvability/analysis/avida_analysis_final_dom.cfg --replicates 100 --run_analysis
