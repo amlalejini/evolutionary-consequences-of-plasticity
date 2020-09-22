cc514b5cd04a0a72abea01a57e8e32e4c6c9da61
Alexander Lalejini <amlalejini@gmail.com>
2020-09-22 11:06:08 -0400

    finish drafting 2020-09-21 experiment submission and configuration

--- experiments/2020-09-21-complexity/hpcc/gen_sub_multi.py
+++ experiments/2020-09-21-complexity/hpcc/gen_sub_multi.py
@@ -5,9 +5,9 @@ Generate slurm job submission scripts - one per condition
 import argparse, os, sys, errno, subprocess, csv
 from pyvarco import CombinationCollector
 
-seed_offset = 21000
+seed_offset = 30000
 default_num_replicates = 30
-job_time_request = "96:00:00"
+job_time_request = "36:00:00"
 job_memory_request = "4G"
 job_name = "avida"
 executable = "avida"
@@ -17,16 +17,24 @@ base_script_filename = './base_script.txt'
 combos = CombinationCollector()
 combos.register_var('EVENT_FILE_PREFIX')
 combos.register_var('SENSORS')
-combos.add_val('EVENT_FILE_PREFIX', [\
-        'events-ALL-u0',        \
-        'events-A-u0',        \
-        'events-B-u0',        \
-        'events-chg-u3',          \
-        'events-chg-u10',          \
-        'events-chg-u100',          \
-        'events-chg-u300',          \
-        'events-chg-u1000'
-    ])
+combos.register_var('ENVIRONMENT')
+combos.add_val(
+    'EVENT_FILE_PREFIX',
+    [
+        'events-ALL-u0',
+        'events-A-u0',
+        'events-B-u0',
+        'events-chg-u30',
+        'events-chg-u100'
+    ]
+)
+combos.add_val(
+    'ENVIRONMENT',
+    [
+        'L9',
+        'L77'
+    ]
+)
 combos.add_val('SENSORS', ['0','1'])
 combos.add_exception({'EVENT_FILE_PREFIX': 'events-ALL-u0', 'SENSORS' : '0'})
 combos.add_exception({'EVENT_FILE_PREFIX': 'events-A-u0', 'SENSORS' : '0'})
@@ -53,7 +61,6 @@ def main():
     parser.add_argument("--phase_two_dir", type=str, help="Where is the output directory for phase two of each run?")
     parser.add_argument("--config_dir", type=str, help="Where is the configuration directory for experiment?")
     parser.add_argument("--replicates", type=int, default=default_num_replicates, help="How many replicates should we run of each condition?")
-    parser.add_argument("--analysis_file", type=str, default="analysis.cfg", help="Path to the analysis script to use for avida analyze mode.")
     parser.add_argument("--job_dir", type=str, help="Where to output these job files?")
 
     # Load in command line arguments
@@ -62,7 +69,6 @@ def main():
     phase_two_dir = args.phase_two_dir
     config_dir = args.config_dir
     num_replicates = args.replicates
-    analysis_file_path = args.analysis_file
     job_dir = args.job_dir
 
     # Get list of all combinations to run
@@ -75,10 +81,11 @@ def main():
 
     # Create job file for each condition
     cur_job_id = 0
+    cond_i = 0
     for condition_dict in combo_list:
         cur_seed = seed_offset + (cur_job_id * num_replicates)
         job_id_str = str(cur_seed).zfill(num_digits)
-        filename_prefix = f'{cur_seed}_avida__{combos.get_str(condition_dict)}'
+        filename_prefix = f'RUN_C{cond_i}'
         file_str = base_sub_script
         file_str = file_str.replace("<<TIME_REQUEST>>", job_time_request)
         file_str = file_str.replace("<<ARRAY_ID_RANGE>>", f"1-{num_replicates}")
@@ -88,28 +95,31 @@ def main():
         file_str = file_str.replace("<<EXEC>>", executable)
         file_str = file_str.replace("<<JOB_SEED_OFFSET>>", str(cur_seed))
 
-        #### Phase 1
+        # ===================================================
+        # ===================== Phase 1 =====================
+        # ===================================================
         file_str = file_str.replace("<<PHASE_1_DIR>>", \
-            os.path.join(phase_one_dir, f'{filename_prefix}', '${SEED}'))
+            os.path.join(phase_one_dir, f'{filename_prefix}_'+'${SEED}'))
         # Format configuration parameters for the run
-        run_params =  f'-set EVENT_FILE {condition_dict["EVENT_FILE_PREFIX"] + "_phase-one.cfg"}'
+        run_params =  f'-set EVENT_FILE {condition_dict["EVENT_FILE_PREFIX"]}_phase-one.cfg'
+        run_params += f' -set ENVIRONMENT_FILE environment-{condition_dict["ENVIRONMENT"]}_phase-one.cfg'
         run_params +=  ' -set COPY_MUT_PROB 0.0025'
         run_params += f' -set DISABLE_REACTION_SENSORS {condition_dict["SENSORS"]}'
         run_params +=  ' -set REACTION_SENSORS_NEUTRAL 0.0'
         run_params +=  ' -set PHYLOGENY_SNAPSHOT_RES 200000'
         run_params +=  ' -set RANDOM_SEED ${SEED}'
-        # Add run commands if we're running the experiment.
+        # Add run commands to run the experiment
         run_commands = ''
         run_commands += f'RUN_PARAMS="{run_params}"\n'
         run_commands += 'echo "./${EXEC} ${RUN_PARAMS}" > cmd.log\n'
         run_commands += './${EXEC} ${RUN_PARAMS} > run.log\n'
         run_commands += 'mv ./*.csv ./data/ \n'
         file_str = file_str.replace("<<PHASE_1_RUN_COMMANDS>>", run_commands)
-        # Add analysis commands if we're analyzing the data
+        # Add commands to run avida analyze mode
         analysis_commands = ''
         analysis_commands += f'RUN_PARAMS="{run_params}"\n'
         analysis_commands += './${EXEC} ${RUN_PARAMS}'
-        analysis_commands += ' -set ANALYZE_FILE ' + analysis_file_path
+        analysis_commands += f' -set ANALYZE_FILE avida-analysis-{condition_dict["ENVIRONMENT"]}.cfg'
         analysis_commands += ' -a\n'
         file_str = file_str.replace("<<PHASE_1_ANALYSIS_COMMANDS>>", analysis_commands)
 
@@ -143,34 +153,39 @@ def main():
             transition_commands += 'cp ${PHASE_1_DIR}/data/analysis/env_all/final_dominant.gen ./seed_org.gen\n'
         file_str = file_str.replace('<<PHASE_TRANSITION>>', transition_commands)
 
-        #### Phase 2
+        # ===================================================
+        # ===================== Phase 2 =====================
+        # ===================================================
         file_str = file_str.replace("<<PHASE_2_DIR>>", \
-            os.path.join(phase_two_dir, f'{filename_prefix}', '${SEED}'))
+            os.path.join(phase_two_dir, f'{filename_prefix}_'+'${SEED}'))
         # Format configuration parameters for the run
-        run_params =  f'-set EVENT_FILE {condition_dict["EVENT_FILE_PREFIX"] + "_phase-two.cfg"}'
+        run_params =  f'-set EVENT_FILE {condition_dict["EVENT_FILE_PREFIX"]}_phase-two.cfg'
+        run_params += f' -set ENVIRONMENT_FILE environment-{condition_dict["ENVIRONMENT"]}_phase-two.cfg'
         run_params +=  ' -set COPY_MUT_PROB 0.0025'
         run_params += f' -set DISABLE_REACTION_SENSORS {condition_dict["SENSORS"]}'
         run_params +=  ' -set REACTION_SENSORS_NEUTRAL 0.0'
         run_params +=  ' -set PHYLOGENY_SNAPSHOT_RES 200000'
         run_params +=  ' -set RANDOM_SEED ${SEED}'
-        # Add run commands if we're running the experiment.
+        # Add run commands to run the experiment
         run_commands = ''
         run_commands += f'RUN_PARAMS="{run_params}"\n'
         run_commands += 'echo "./${EXEC} ${RUN_PARAMS}" > cmd.log\n'
         run_commands += './${EXEC} ${RUN_PARAMS} > run.log\n'
         run_commands += 'mv ./*.csv ./data/ \n'
         file_str = file_str.replace("<<PHASE_2_RUN_COMMANDS>>", run_commands)
-        # Add analysis commands if we're analyzing the data
+        # Add commands to run avida analyze mode
         analysis_commands = ''
         analysis_commands += f'RUN_PARAMS="{run_params}"\n'
         analysis_commands += './${EXEC} ${RUN_PARAMS}'
-        analysis_commands += ' -set ANALYZE_FILE ' + analysis_file_path
+        analysis_commands += f' -set ANALYZE_FILE avida-analysis-{condition_dict["ENVIRONMENT"]}.cfg'
         analysis_commands += ' -a\n'
         file_str = file_str.replace("<<PHASE_2_ANALYSIS_COMMANDS>>", analysis_commands)
 
+        mkdir_p(job_dir)
         with open(os.path.join(job_dir, f'{filename_prefix}.sb'), 'w') as fp:
             fp.write(file_str)
         cur_job_id += 1
+        cond_i += 1
 
 if __name__ == "__main__":
     main()
--- experiments/2020-09-21-complexity/hpcc/job_gen.sh
+++ experiments/2020-09-21-complexity/hpcc/job_gen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-python3 gen_sub_multi.py --config_dir /mnt/home/fergu358/research/plasticity/evolvability/experiments/2020-09-03-pop-dynamics-two-phase/hpcc/config/ --phase_one_dir /mnt/research/devolab/fergujini/2020-09-03-pop-dynamics-two-phase/data_phase_1/ --phase_two_dir /mnt/research/devolab/fergujini/2020-09-03-pop-dynamics-two-phase/data_phase_2/ --job_dir /mnt/research/devolab/fergujini/2020-09-03-pop-dynamics-two-phase/jobs/ --analysis_file /mnt/home/fergu358/research/plasticity/evolvability/experiments/2020-09-03-pop-dynamics-two-phase/hpcc/avida_analysis_final_dom.cfg --replicates 100
 
-#python3 gen_sub_multi.py 
-    # --config_dir /mnt/home/fergu358/research/plasticity/evolvability/experiments/2020-09-03-pop-dynamics-two-phase/hpcc/config/ 
-    # --phase_one_dir /mnt/research/devolab/fergujini/2020-09-03-pop-dynamics-two-phase/data_phase_1/ 
-    # --phase_two_dir /mnt/research/devolab/fergujini/2020-09-03-pop-dynamics-two-phase/data_phase_2/ 
-    # --job_dir /mnt/research/devolab/fergujini/2020-09-03-pop-dynamics-two-phase/jobs/ 
-    # --analysis_file /mnt/home/fergu358/research/plasticity/evolvability/experiments/2020-09-03-pop-dynamics-two-phase/hpcc/avida_analysis_final_dom.cfg 
-    # --replicates 100
+python3 gen_sub_multi.py --config_dir /mnt/home/lalejini/devo_ws/plastic-evolvability-avida/experiments/2020-09-21-complexity/hpcc/config/ --phase_one_dir /mnt/research/devolab/fergujini/2020-09-21-complexity/phase_1/ --phase_two_dir /mnt/research/devolab/fergujini/2020-09-21-complexity/phase_2/ --job_dir /mnt/research/devolab/fergujini/2020-09-21-complexity/jobs/ --replicates 50
+
+# python3 gen_sub_multi.py
+    # --config_dir /mnt/home/lalejini/devo_ws/plastic-evolvability-avida/experiments/2020-09-21-complexity/hpcc/config/
+    # --phase_one_dir /mnt/research/devolab/fergujini/2020-09-21-complexity/phase_1/
+    # --phase_two_dir /mnt/research/devolab/fergujini/2020-09-21-complexity/phase_2/
+    # --job_dir /mnt/research/devolab/fergujini/2020-09-21-complexity/jobs/
+    # --replicates 50
