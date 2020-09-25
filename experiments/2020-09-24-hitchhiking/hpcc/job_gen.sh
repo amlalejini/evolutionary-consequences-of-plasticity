341c7f3f214cc57a233e93562cdd2914986bbadf
Alexander Lalejini <amlalejini@gmail.com>
2020-09-25 12:08:40 -0400

    add job_gen script to 2020-09-24 experiment

---
+++ experiments/2020-09-24-hitchhiking/hpcc/job_gen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 gen_sub_multi.py --config_dir /mnt/home/lalejini/devo_ws/plastic-evolvability-avida/experiments/2020-09-24-hitchhiking/hpcc/config/ --phase_one_dir /mnt/research/devolab/fergujini/2020-09-24-hitchhiking/phase_1/ --phase_two_dir /mnt/research/devolab/fergujini/2020-09-24-hitchhiking/phase_2/ --job_dir /mnt/research/devolab/fergujini/2020-09-24-hitchhiking/jobs/ --replicates 100
+
+# python3 gen_sub_multi.py
+    # --config_dir /mnt/home/lalejini/devo_ws/plastic-evolvability-avida/experiments/2020-09-24-hitchhiking/hpcc/config/
+    # --phase_one_dir /mnt/research/devolab/fergujini/2020-09-24-hitchhiking/phase_1/
+    # --phase_two_dir /mnt/research/devolab/fergujini/2020-09-24-hitchhiking/phase_2/
+    # --job_dir /mnt/research/devolab/fergujini/2020-09-24-hitchhiking/jobs/
+    # --replicates 100
