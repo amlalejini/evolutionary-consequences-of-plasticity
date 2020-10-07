c08aa6c5a6d6730a7a57717317720bed1d380095
Alexander Lalejini <amlalejini@gmail.com>
2020-10-07 17:38:28 -0400

    draft 2020-10-07 complexity experiment

---
+++ experiments/2020-10-07-complexity/hpcc/job_gen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 gen_sub_multi.py --config_dir /mnt/home/lalejini/devo_ws/plastic-evolvability-avida/experiments/2020-10-07-complexity/hpcc/config/ --phase_one_dir /mnt/scratch/lalejini/data/avida-plasticity/2020-10-07-complexity/phase-1 --phase_two_dir /mnt/scratch/lalejini/data/avida-plasticity/2020-10-07-complexity/phase-1 --job_dir /mnt/scratch/lalejini/data/avida-plasticity/2020-10-07-complexity/jobs --replicates 100
+
+# python3 gen_sub_multi.py
+    # --config_dir /mnt/home/lalejini/devo_ws/plastic-evolvability-avida/experiments/2020-10-07-complexity/hpcc/config/
+    # --phase_one_dir /mnt/scratch/lalejini/data/avida-plasticity/2020-10-07-complexity/phase-1
+    # --phase_two_dir /mnt/scratch/lalejini/data/avida-plasticity/2020-10-07-complexity/phase-1
+    # --job_dir /mnt/scratch/lalejini/data/avida-plasticity/2020-10-07-complexity/jobs
+    # --replicates 100
