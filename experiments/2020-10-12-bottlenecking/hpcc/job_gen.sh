79d64aa0b38a057cc59563a89e3b64da96d16868
Alexander Lalejini <amlalejini@gmail.com>
2020-10-12 17:57:55 -0400

    draft hpcc submission scripts/configs for 2020-10-12 bottlenecking experiment

---
+++ experiments/2020-10-12-bottlenecking/hpcc/job_gen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 gen_sub_multi.py --config_dir /mnt/home/lalejini/devo_ws/plastic-evolvability-avida/experiments/2020-10-12-bottlenecking/hpcc/config/ --phase_one_dir /mnt/scratch/lalejini/data/avida-plasticity/2020-10-12-bottlenecking/phase-1 --phase_two_dir /mnt/scratch/lalejini/data/avida-plasticity/2020-10-12-bottlenecking/phase-2 --job_dir /mnt/scratch/lalejini/data/avida-plasticity/2020-10-12-bottlenecking/jobs --replicates 100
+
+# python3 gen_sub_multi.py
+    # --config_dir /mnt/home/lalejini/devo_ws/plastic-evolvability-avida/experiments/2020-10-12-bottlenecking/hpcc/config/
+    # --phase_one_dir /mnt/scratch/lalejini/data/avida-plasticity/2020-10-12-bottlenecking/phase-1
+    # --phase_two_dir /mnt/scratch/lalejini/data/avida-plasticity/2020-10-12-bottlenecking/phase-2
+    # --job_dir /mnt/scratch/lalejini/data/avida-plasticity/2020-10-12-bottlenecking/jobs
+    # --replicates 100
