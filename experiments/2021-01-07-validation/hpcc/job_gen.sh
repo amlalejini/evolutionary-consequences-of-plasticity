9ee5f1e671b18736aad583eb20e9c380180daff5
Alexander Lalejini <amlalejini@gmail.com>
2021-01-07 18:01:37 -0500

    fix experiment name in 2021-01-07 job gen script

--- experiments/2021-01-07-validation/hpcc/job_gen.sh
+++ experiments/2021-01-07-validation/hpcc/job_gen.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-CONFIG_DIR=/mnt/home/lalejini/devo_ws/plastic-evolvability-avida/experiments/2021-01-07/hpcc/config
-DATA_DIR=/mnt/scratch/lalejini/data/avida-plasticity/2021-01-07
+EXP_TAG=2021-01-07-validation
+CONFIG_DIR=/mnt/home/lalejini/devo_ws/plastic-evolvability-avida/experiments/${EXP_TAG}/hpcc/config
+DATA_DIR=/mnt/scratch/lalejini/data/avida-plasticity/${EXP_TAG}
 REPLICATES=100
-ANALYSIS_FILE=/mnt/home/lalejini/devo_ws/plastic-evolvability-avida/experiments/2021-01-07/hpcc/avida_analysis_final_dom.cfg
+ANALYSIS_FILE=/mnt/home/lalejini/devo_ws/plastic-evolvability-avida/experiments/${EXP_TAG}/hpcc/avida_analysis_final_dom.cfg
 
 python3 gen-sub.py --config_dir ${CONFIG_DIR} --data_dir ${DATA_DIR} --replicates ${REPLICATES} --run_experiment --run_analysis --analysis_file ${ANALYSIS_FILE}
 
